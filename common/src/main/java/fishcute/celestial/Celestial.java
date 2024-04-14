a7c2ed77264d99e376793933eea5e00be92cbc2c
block36_ <block36not33@gmail.com>
2024-04-14 10:33:16 -0400

    Init Instance of VRenderSystem

--- common/src/main/java/fishcute/celestial/Celestial.java
+++ common/src/main/java/fishcute/celestial/Celestial.java
@@ -1,10 +1,16 @@
 package fishcute.celestial;
 
+import fishcute.celestialmain.version.independent.Instances;
+
 public class Celestial
 {
 	public static final String MOD_ID = "celestial";
 
 	public static void init() {
-		
+		initInstances();
+	}
+
+	public static void initInstances() {
+		Instances.renderSystem = new VRenderSystem();
 	}
 }
