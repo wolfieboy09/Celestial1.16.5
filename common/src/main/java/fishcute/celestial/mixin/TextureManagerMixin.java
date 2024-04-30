04ba67a89a0862c4cfe927f0959f7950248dc5f4
fishcute <47741160+fishcute@users.noreply.github.com>
2024-04-30 17:33:41 -0400

    Frogot

--- common/src/main/java/fishcute/celestial/mixin/TextureManagerMixin.java
+++ common/src/main/java/fishcute/celestial/mixin/TextureManagerMixin.java
@@ -2,23 +2,18 @@ package fishcute.celestial.mixin;
 
 import fishcute.celestialmain.util.ClientTick;
 import net.minecraft.client.renderer.texture.TextureManager;
-import net.minecraft.server.packs.repository.Pack;
-import net.minecraft.server.packs.repository.PackRepository;
 import net.minecraft.server.packs.resources.PreparableReloadListener;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.*;
 
 @Mixin(TextureManager.class)
-public class PackRepositoryMixin {
+public class TextureManagerMixin {
     @Inject(method = "reload", at = @At("RETURN"))
     private void reload(PreparableReloadListener.PreparationBarrier preparationBarrier, ResourceManager resourceManager, ProfilerFiller profilerFiller, ProfilerFiller profilerFiller2, Executor executor, Executor executor2, CallbackInfoReturnable<CompletableFuture<Void>> cir) {
         ClientTick.onReload();
